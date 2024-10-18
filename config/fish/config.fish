169fe714e5673247add2a191ccaa27b2bfe9c664
Maxime Ayaman <kayamaox@gmail.com>
2024-10-18 18:37:34 +0000

    Add files via upload

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,19 @@
+# Alias
+#############
+alias cr "composer require "
+alias apps "cd ~/Apps"
+alias phpb "php bin/console "
+alias spc "sudo pacman -S "
+alias spr "sudo pacman -R "
+alias mi "makepkg -si "
+alias mr "makepkg -r "
+alias admin "sudo -s "
+alias cls "clear"
+alias home "cd ~"
+alias la "ls -alF"
+alias files "thunar"
+alias mkd "mkdir -m 755 "
+alias mki "mkdir -m 777 "
 
+# Laravel
+alias phpa "php artisan"
